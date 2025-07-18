b5206a58586d8ffeff6c3c8475cb30af936fbf93
Harish Prajapat <122553323+Harish-Prajapat@users.noreply.github.com>
2025-07-19 02:11:39 +0530

    Update userRoutes.js

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {createUser, getUser , getAllUser, deleteUser} from '../Controllers/userControllers.js'
+import {createUser} from '../Controllers/userControllers.js'
 const Router = express.Router()
 
 Router.post('/createUser', createUser)
